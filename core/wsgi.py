bf4fc6d642cb8f9d7607d4c512f1fcc30c7d7913
sakio87 <guilherme-oyama87@hotmail.com>
2026-08-16 12:40:53 -0300

    Atualiza criacao de superuser no wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 from django.contrib.auth import get_user_model
@@ -15,10 +6,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-# esse codigo ele Cria o superusuário automaticamente no deploy gratuito do Render
+# Garante a criação ou atualização do superusuário no Render
 try:
     User = get_user_model()
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    else:
+        # esse codigo garante Se já existir, garante que a senha está correta
+        admin_user = User.objects.get(username='admin')
+        admin_user.set_password('1234')
+        admin_user.is_superuser = True
+        admin_user.is_staff = True
+        admin_user.save()
 except Exception:
     pass
