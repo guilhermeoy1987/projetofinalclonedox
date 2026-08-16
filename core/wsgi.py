3a2e724872224025fe380abde62dc8987b8e81ce
sakio87 <guilherme-oyama87@hotmail.com>
2026-08-16 12:32:06 -0300

    Cria superuser automatico no wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# esse codigo ele Cria o superusuário automaticamente no deploy gratuito do Render
+try:
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+except Exception:
+    pass
